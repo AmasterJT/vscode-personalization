71eeea52805f378626adcd50ecb719e1a334a8ee
AmasterJT <amasterjt@gmail.com>
2024-09-09 14:37:59 +0200

    creamos script para sincronizar los settings de vscode

---
+++ refresh-json-settings-windows.ps1
@@ -0,0 +1,2 @@
+Copy-Item C:\Users\'Jose Tomas'\AppData\Roaming\Code\User\settings.json  D:\Documentos\My-Settings\VsCode\.vscode\my-settings.json
+Copy-Item C:\Users\'Jose Tomas'\AppData\Roaming\Code\User\keybindings.json  D:\Documentos\My-Settings\VsCode\.vscode\my-keybindings.json
